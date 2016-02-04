5866b1c0f03d696586c4f36944bd76520a0c37af
Oliver K <oliverk120@gmail.com>
2016-02-04 18:32:15 -0500

    got basic url params to work

--- app.js
+++ app.js
@@ -1,38 +1,57 @@
 var app = angular.module('creativegifts', [ 'ui.bootstrap', 'ui.router'])
 
 .config([
-'$stateProvider',
-'$urlRouterProvider',
-function($stateProvider, $urlRouterProvider) {
-  console.log($urlRouterProvider);
-  $stateProvider
+  '$stateProvider',
+  '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider) {
+    console.log($urlRouterProvider);
+    $stateProvider
     .state('home', {
       url: '/home',
       templateUrl: '/home.html',
       controller: 'MainCtrl'
     })
     .state('gifts', {
-      url: '/gifts',
+      url: '/gifts/{id}',
       templateUrl: '/gifts.html',
       controller: 'GiftsCtrl'
     });
 
-  $urlRouterProvider.otherwise('home');
+    $urlRouterProvider.otherwise('home');
+  }])
+
+app.factory('gifts', [function(){
+  var o = {
+    gifts: []
+  };
+  return o;
 }])
 
-.controller('MainCtrl', ['$scope',
-function($scope){
-      //all query items added in view other than default min/max price
-    $scope.query = {};
-    $scope.query.minprice = 0;
-    $scope.query.maxprice = 400;   
-}])
+.controller('MainCtrl', ['$scope', 'gifts',
+  function($scope, gifts){
+    $scope.gifts = gifts.gifts;
+    //fake gift info
+  
 
-.controller('GiftsCtrl', ['$scope',
-function($scope){
+    $scope.gifts.push({title: '$scope.title'},{title: '$scoasdfitle'},{title: '$scofweewwfetle'},{title: '$scowfefwewetle'});
+
+
       //all query items added in view other than default min/max price
-    $scope.gift = {name:'first gift'};
-}]);
+      $scope.query = {};
+      $scope.query.minprice = 0;
+      $scope.query.maxprice = 400;   
+    }])
+
+.controller('GiftsCtrl', [
+  '$scope',
+  '$stateParams',
+  'gifts',
+  function($scope, $stateParams, gifts){
+    
+      //all query items added in view other than default min/max price
+      $scope.gift = gifts.gifts[$stateParams.id];
+      $scope.othergift = gifts;
+    }]);
 
 
 
